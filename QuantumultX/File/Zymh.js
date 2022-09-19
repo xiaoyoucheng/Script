c7bd67487699dab377be215018d99110fe831e1c
xiaoyoucheng <33143581+xiaoyoucheng@users.noreply.github.com>
2022-09-19 20:30:55 +0800

    Update Zymh.js
    修改

--- QuantumultX/File/Zymh.js
+++ QuantumultX/File/Zymh.js
@@ -25,6 +25,8 @@ obj.status = 0;
 obj.data.isvip = 1;
 obj.data.coins = 6666;
 obj.data.Cgold = 6666;
+obj.data.Cexp = 6666;
+obj.data.ismkxq = 1;
 obj.data.kmh_coins = 6666;
 obj.data.star_coin = 6666;
 $done({body: JSON.stringify(obj)});
